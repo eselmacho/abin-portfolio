c83942ee4a3c04b688fcb2a8dc2644f076bdb051
Abin <abhinsethu@gmail.com>
2026-02-23 20:54:44 +0530

    Initial portfolio version

---
+++ components/Marquee.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import styles from "./Marquee.module.css";
+
+export default function Marquee() {
+  const text =
+    "This site is still evolving. If something feels odd, it probably is. Expect rough edges, as some pixels are still thinking.";
+
+  return (
+    <div className="relative w-2xl">
+      <div className={styles.mask}>
+        <div className={styles.track}>
+          <span className={`${styles.text} text-2xl font-medium text-neutral-400`}>
+            {text}
+          </span>
+          <span className={`${styles.text} text-2xl font-medium text-neutral-400`}>
+            {text}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,23 @@
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 w-full flex justify-between items-center px-8 py-6 text-white">
+      
+      <div className="text-xl font-medium text-neutral-400">
+        ABIN S L
+      </div>
+
+      <div className="space-x-8 text-lg text-neutral-400">
+        <a href="#" className="hover:text-neutral-700 transition-colors">
+          Work
+        </a>
+        <a href="#" className="hover:text-neutral-700 transition-colors">
+          About
+        </a>
+        <a href="#" className="hover:text-neutral-700 transition-colors">
+          Connect
+        </a>
+      </div>
+
+    </nav>
+  )
+}
